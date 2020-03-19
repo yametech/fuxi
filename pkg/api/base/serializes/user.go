19b9b30c71a09eafb73bc6f2203548aae26b7c5f
laik <laik.lj@me.com>
2020-03-19 22:47:41 +0800

    fix failed from deployment resource merge

--- pkg/api/base/serializes/user.go
+++ pkg/api/base/serializes/user.go
@@ -2,13 +2,9 @@ package serializes
 
 import "github.com/yametech/fuxi/pkg/db"
 
-<<<<<<< HEAD
-func (u *db.User) UserRoleSerializer() JSON {
-=======
 type User db.User
 
 func (u *User) UserRoleSerializer() JSON {
->>>>>>> 0c71acc6e0202644d124b914f3c302d8c1d93ea5
 	return JSON{
 		"id": u.ID,
 	}
