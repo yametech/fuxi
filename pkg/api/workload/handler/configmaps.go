301ab4931137185128761c9179ea8f7bfbf65c08
pskishere <pskishere@gmail.com>
2020-04-13 11:31:02 +0800

    Add configMaps workload

---
+++ pkg/api/workload/handler/configmaps.go
@@ -0,0 +1,36 @@
+package handler
+
+import (
+	"encoding/json"
+	"github.com/gin-gonic/gin"
+	dyn "github.com/yametech/fuxi/pkg/kubernetes/client"
+	"k8s.io/api/core/v1"
+	"net/http"
+)
+
+// Get ConfigMaps
+func (w *WorkloadsAPI) GetConfigMaps(g *gin.Context) {
+	namespace := g.Param("namespace")
+	name := g.Param("name")
+	item, err := w.configMaps.Get(dyn.ResourceConfigMaps, namespace, name)
+	if err != nil {
+		g.JSON(http.StatusBadRequest,
+			gin.H{code: http.StatusBadRequest, data: "", msg: err.Error(), status: "Request bad parameter"})
+		return
+	}
+	g.JSON(http.StatusOK, item)
+}
+
+// List ConfigMaps
+func (w *WorkloadsAPI) ListConfigMaps(g *gin.Context) {
+	list, _ := w.configMaps.List(dyn.ResourceConfigMaps, "", "", 0, 100, nil)
+	configMapList := &v1.ConfigMapList{}
+	marshalData, err := json.Marshal(list)
+	if err != nil {
+		g.JSON(http.StatusBadRequest,
+			gin.H{code: http.StatusBadRequest, data: "", msg: err.Error(), status: "Request bad parameter"})
+		return
+	}
+	_ = json.Unmarshal(marshalData, configMapList)
+	g.JSON(http.StatusOK, configMapList)
+}
