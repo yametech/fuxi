becc8fe8ba6e22cb6e1f857d293cb361152639fa
laik <laik.lj@me.com>
2020-07-14 19:20:46 +0800

    fix tekton graph user create / watch problem 2

--- pkg/api/workload/handler/tektongraph.go
+++ pkg/api/workload/handler/tektongraph.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	common_const "github.com/yametech/fuxi/common"
 	"github.com/yametech/fuxi/pkg/api/common"
 	v1 "github.com/yametech/fuxi/pkg/apis/fuxi/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
@@ -32,7 +33,7 @@ func (w *WorkloadsAPI) ListTektonGraph(g *gin.Context) {
 		list, err = w.tektonGraph.List("", "", 0, 0, nil)
 	} else {
 		labelSelector := fmt.Sprintf("namespace=%s", namespace)
-		list, err = w.taskRun.List("", "", 0, 0, labelSelector)
+		list, err = w.tektonGraph.List(common_const.TektonResourceNamespace, "", 0, 0, labelSelector)
 	}
 	if err != nil {
 		common.ToInternalServerError(g, "", err)
