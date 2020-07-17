ba903e93ea201ccfaab7b25ddae121c295abddfb
pskishere <pskishere@gmail.com>
2020-07-17 13:38:26 +0800

    add ops secret

--- cmd/workload/secret.go
+++ cmd/workload/secret.go
@@ -14,7 +14,19 @@ import "github.com/gin-gonic/gin"
 // @Router /workload/api/v1/secrets [get]
 func SecretList(g *gin.Context) { workloadsAPI.ListSecret(g) }
 
-// ConfigMap doc
+// Secret doc
+// @Summary workload secret list
+// @Description workload service for list secret
+// @Tags Secret
+// @Accept mpfd
+// @Produce json
+// @Param namespace query string true "namespace"
+// @Success 200 {string} string "{"msg": "Success"}"
+// @Failure 400 {string} string "{"msg": "Failed"}"
+// @Router /workload/api/v1/ops-secrets [get]
+func OpsSecretList(g *gin.Context) { workloadsAPI.ListOpsSecret(g) }
+
+// Secret doc
 // @Summary workload secret get
 // @Description workload service for get a secret detail
 // @Tags ConfigMap
@@ -27,7 +39,7 @@ func SecretList(g *gin.Context) { workloadsAPI.ListSecret(g) }
 // @Router /workload/api/v1/namespaces/{namespace}/secrets/{name} [get]
 func SecretGet(g *gin.Context) { workloadsAPI.GetSecret(g) }
 
-// ConfigMap doc
+// Secret doc
 // @Summary workload secret get
 // @Description workload service for get a secret detail
 // @Tags ConfigMap
